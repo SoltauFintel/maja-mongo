d7b2376918386f6e100843756bbca70ec0fa93e5
Marcus Warm <warm@mwvb.de>
2017-11-30 22:22:41 +0100

    ds -> ds(), neue Methoden, MongoPlugin

--- maja-mongo/src/main/java/de/mwvb/maja/mongo/AbstractDAO.java
+++ maja-mongo/src/main/java/de/mwvb/maja/mongo/AbstractDAO.java
@@ -6,41 +6,68 @@ import java.util.UUID;
 import java.util.zip.CRC32;
 
 import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.query.Query;
 
 public abstract class AbstractDAO<E> {
-	protected final Datastore ds;
-	protected final Class<E> cls;
 	
-	public AbstractDAO(Database database, Class<E> cls) {
-		this.ds = database == null ? null /* for testing */ : database.ds();
-		this.cls = cls;
+	/**
+	 * @return entity class
+	 */
+	protected abstract Class<E> getCls();
+	
+	public void save(E entity) {
+		ds().save(entity);
 	}
 
-	public void save(E entity) {
-		ds.save(entity);
+	public void delete(E entity) {
+		ds().delete(entity);
+	}
+	
+	/**
+	 * @return all entities of the collection
+	 */
+	public List<E> list() {
+		return createQuery().asList();
+	}
+
+	/**
+	 * Find by id
+	 * 
+	 * @param id String
+	 * @return null if not exists
+	 */
+	public E get(String id) {
+		return createQuery().field("id").equal(id).get();
+	}
+
+	/**
+	 * @return number of entities in collection
+	 */
+	public long size() {
+		return createQuery().count();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<String> distinct(String fieldname) {
+		return new ArrayList<String>(ds().getCollection(getCls()).distinct(fieldname));
+	}
+	
+	protected final Query<E> createQuery() {
+		return ds().createQuery(getCls());
+	}
+
+	protected final Datastore ds() {
+		return MongoPlugin.database.ds();
 	}
 
 	public static String genId() {
-    	return UUID.randomUUID().toString().replace("-", "");
-    }
+		return UUID.randomUUID().toString().replace("-", "");
+	}
 
 	public static String code6(String str) {
 		CRC32 crc = new CRC32();
 		crc.update(str.getBytes());
 		String ret = "000000" + Integer.toString((int) crc.getValue(), 36).toLowerCase().replace("-", "");
 		return ret.substring(ret.length() - 6);
-	}
-	
-	public long size() {
-		return ds.createQuery(cls).count();
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<String> distinct(String fieldname) {
-		return new ArrayList<String>(ds.getCollection(cls).distinct(fieldname));
-	}
-	
-	public void delete(E entity) {
-		ds.delete(entity);
 	}
 }
